b9de5fc02deccdeddf9dcc9e0246ed715797cbd6
wallman1 <william.coote5@education.nsw.gov.au>
2024-03-04 21:26:45 +1100

    Update definitions and fucntions.py

--- definitions and fucntions.py
+++ definitions and fucntions.py
@@ -1,7 +1,9 @@
-def data_show(number, kind):
-    print(f"{kind} has {number} instances")
-data_show(7, "water")
-
+#functions allow you to compress peices of code that may need to be used multiple times
 def do_smth():
     print("this did something")
 do_smth()
+
+#functions can have parameters that can be called on as a variable when writing the code within the function and will be defined when called
+def data_show(number, kind):
+    print(f"{kind} has {number} instances")
+data_show(7, "water")
