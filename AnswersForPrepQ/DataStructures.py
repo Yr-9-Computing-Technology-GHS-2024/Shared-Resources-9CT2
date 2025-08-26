795bcbc4a5fdcad977b019b3f21d0001ae521d6c
wallman1 <william.coote5@education.nsw.gov.au>
2025-08-26 10:07:29 +1000

    Finished the first lot of non extension qs

--- AnswersForPrepQ/DataStructures.py
+++ AnswersForPrepQ/DataStructures.py
@@ -44,3 +44,70 @@ elif inp == 3:
     print("Goodbye")
 else:
     print("Invalid input")
+#Activity 4
+class TreeNode:
+  def __init__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
+
+root = TreeNode(50)
+NodeA = TreeNode(30)
+NodeB = TreeNode(70)
+NodeC = TreeNode(20)
+NodeD = TreeNode(40)
+NodeE = TreeNode(60)
+NodeF = TreeNode(80)
+
+root.left = NodeA
+root.right = NodeB
+
+NodeA.left = NodeC
+NodeA.right = NodeD
+
+NodeB.left = NodeE
+NodeB.right = NodeF
+print("Binary Tree", root.right.left.data, root.right.right.data)
+#Activity 5
+stack = []
+ans = input()
+if ans != "undo" or "quit":
+    stack.append(ans)
+while ans != "quit":
+    ans = input()
+    if ans == "undo":
+        print("Undone")
+        stack.pop()
+    elif ans == "stack":
+        print(stack)
+    else:
+        stack.append(ans)
+#Activity 6
+phonebook = {
+    "John Doe": 245263678,
+    "Bob Bob": 123456789,
+    "Saul Goodman": 911
+}
+
+choice = input("Would you like to add, remove, search or sort")
+choice = choice.lower()
+if choice == "add":
+    added = input("Please type the name of the person")
+    add = input("Please type their number")
+    phonebook[added] = add
+elif choice == "remove":
+    remove = input("Please type the name of the person")
+    try:
+        phonebook.pop(remove)
+    except:
+        print("Invalid choice")
+elif choice == "search":
+    name = input("Please type the name of the person")
+    try:
+        x = phonebook.get(name)
+        print(x)
+    except:
+        print("Name not found")
+elif choice == "sort":
+    y = dict(sorted(phonebook.items()))
+    print(y)
